1d0ccf405922040bdf6e2cf675dd2241bba9a057
beanjs <445212619@qq.com>
2022-11-12 17:43:54 +0800

    fix

--- rgb-leds/index.js
+++ rgb-leds/index.js
@@ -5,7 +5,31 @@ const beanio = require('beanio') // beanio 模块
 const PIN_R = B0
 const PIN_G = B1
 const PIN_B = B2
-const SUBJECT_COLOR = 'w800.color.rgb' // 数据通道名称，长度需小于32
+const SUBJECT_COLOR = 'color-picker.rgb' // 数据通道名称，长度需小于32
+
+// 程序入口
+function onInit () {
+  PIN_R.mode('output_pulldown')
+  PIN_G.mode('output_pulldown')
+  PIN_B.mode('output_pulldown')
+}
+
+// 设置 beanio 连接成功 事件函数
+beanio.on('connect', () => console.log('beanio connected'))
+
+// 设置 beanio 连接断开 事件函数
+beanio.on('disconnect', () => console.log('beanio disconnected'))
+
+// 设置 beanio 数据通道 事件函数
+beanio.on(SUBJECT_COLOR, rgb => {
+  const r = (rgb >> 16) & 0xff
+  const g = (rgb >> 8) & 0xff
+  const b = (rgb >> 0) & 0xff
+
+  analogWrite(PIN_R, r / 255, { freq: 100 })
+  analogWrite(PIN_G, g / 255, { freq: 100 })
+  analogWrite(PIN_B, b / 255, { freq: 100 })
+})
 
 // 设置 beanio 参数
 beanio.setup({
@@ -16,21 +40,3 @@ beanio.setup({
     password: ENV_PSWD // WIFI 密码
   }
 })
-
-// 设置 beanio 连接成功 事件函数
-beanio.on('connect', () => console.log('beanio connected'))
-
-// 设置 beanio 连接断开 事件函数
-beanio.on('disconnect', () => console.log('beanio disconnected'))
-
-// 设置 beanio 数据通道 事件函数
-beanio.on(SUBJECT_COLOR, val => {
-  console.log({ color: val })
-})
-
-// 程序入口
-function onInit () {
-  PIN_R.mode('output_pulldown')
-  PIN_G.mode('output_pulldown')
-  PIN_B.mode('output_pulldown')
-}
